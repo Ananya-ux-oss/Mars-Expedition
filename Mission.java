c06a17378fb8dcfe2facf3b1f8e8d7bf1a6a49fb
Ananya-ux-oss <Ananyalemma@gmail.com>
2026-05-29 04:43:22 -0700

    Update Mission.java

--- Mission.java
+++ Mission.java
@@ -3,21 +3,18 @@ public class Mission {
     private String objective;
     private int difficulty;
     private boolean completed;
-    /*private boolean isoperational;
-    private int batterylife; // what is the battery life and durability of the mission?
-    private int durability;*/
-
-    Mission(String missionname, String objective, int difficulty, boolean completed/*boolean isoperational,
-        int batterylife, int durability*/)
+    
+    Mission(String missionname, String objective, int difficulty, boolean completed
+        )
      {
         this.missionname = missionname;
         this.objective = objective;
         this.difficulty = difficulty;
         this.completed = completed;
-        /*this.isoperational = isoperational;
-        this.batterylife = batterylife;
-        this.durability = durability;*/
+        
     }
+
+    
 
     public String getMissionname() {
         return missionname;
@@ -31,52 +28,36 @@ public class Mission {
     public boolean getCompleted() {
         return completed;
     }
-    /*public boolean getIsoperational() {
-        return isoperational;
+
+    public void completeMission(Rover rover, CrewMember person) { //works
+
+        if(rover.getoperations() && person.getStamina() > 35) {
+
+        completed = true;
+
+        System.out.println("Mission completed successfully!");
+
+        rover.decreaseBatteryLife(10);
+        rover.decreaseDurability(15);
+        person.decreaseStamina(10);
     }
-
-    public int getBatterylife() {
-        return batterylife;
+    else {
+        completed = false;
+        System.out.println("Mission failed."); // might need to remove this 
     }
-
-    public int getDurability() {
-        return durability;
-    }*/
-
-    public void Startmission(Rover rover, CrewMember person) {
-        /*if(!isoperational) {
-            System.out.println("Mission cannot start. Rover is not operational.");
-            return;
-        }
-        System.out.println("Mission started successfully.");
-        batterylife -= 10;
-        durability -= 5*/
-        rover.CheckSystemStaus();
-        rover.drive();
-        
-
-    }
-
-    public void completeMission(Rover rover, CrewMember person) {
-    System.out.println("Mission completed successfully!");
-
-    rover.decreaseBatteryLife(10);
-    rover.decreaseDurability(15);
-    person.DecreaseStamina(10);
-
-   }
+}
 
   
   public void displayMission() {
     System.out.println(
-        "=== Mission Status Report ===" +
-        "Mission Name: " + missionname +
-        "Objective: " + objective +
-        "Difficulty: " + difficulty +
-        "Completed: " + completed 
-        /*"Operational: " + isoperational +
-        "Battery Life: " + batterylife +
-        "Durability: " + durability*/
+       "=== Mission Status Report ===" +
+        "\nMission Name: " + missionname +
+        "\nObjective: " + objective +
+        "\nDifficulty: " + difficulty +
+        "\nCompleted: " + completed +
+        "\nCrew stamina: " + person.getStamina() +
+        "\nRover batterylife: "  + rover.getBatterylife()
+        
     );
   }
 }
