c34b12f6f3825fe944d298231459b9ba9f021733
haileyesusdagne-source <haileyesus.dagne@bitscollege.edu.et>
2026-05-29 22:53:22 +0300

    Update MissionControlSystem.java
    Upcasting

--- MissionControlSystem.java
+++ MissionControlSystem.java
@@ -1,13 +1,5 @@
 public class MissionControlSystem {
     public static void main(String[] args) {
-        Scientist scientist1 = new Scientist("alex", 200, 200, 100, 5, "scientist", "geologist", 0);
-
-        scientist1.displayInfo();
-
-        Rover rover1 = new Rover("C3PO", 100, 100, true);
-
-        rover1.displayInfo();
-
-        Mission firstMission = new Mission("mission1", "save george lucas", 90, true);
+        CrewMember member1 = new Pilot("Emilia", 20, 80, 100, 7, "Pilot", 10, 0); //Upcasting
     }
 }
