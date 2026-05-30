14e68f94f307d2bfb4304bb188dd669178b417c6
Ananya-ux-oss <Ananyalemma@gmail.com>
2026-05-29 23:12:35 -0700

    Update MissionControlSystem.java

--- MissionControlSystem.java
+++ MissionControlSystem.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class MissionControlSystem {
     public static void main(String[] args) {
         CrewMember member1 = new Pilot("Emilia", 20, 80, 100, 7, "Pilot", 10, 0); //Upcasting
